300ba30f9f3b6d0a0f905f7b2096fa7699a84626
enigma812 <antiseptic812@gmail.com>
2022-06-16 18:07:54 +0300

    feat(task7) solve snake

--- src/modules/task7/task7/task7-page.component.ts
+++ src/modules/task7/task7/task7-page.component.ts
@@ -13,21 +13,21 @@ export class Task7PageComponent {
 
   public createHelix(str: string): void {
     let n: number = Number.parseInt(str, 10);
-    this.helix = this.getHelix(n)
+    this.helix = this.getSnake(n)
   }
 
 
   private getHelix(n: number): number[][] {
     const result: number[][] = [];
-    for (let i = 0; i < n; i + 1) {
-      for (let j = 0; j < n; j + 1) {
-        if (i = n - 1) {
+    for (let i = 0; i < n; i = i + 1) {
+      for (let j = 0; j < n; j = j + 1) {
+        if (i === n - 1) {
           result[i][j] = result[i * 2 + 1][j];
-          if (i = n * n - 1) {
+          if (i === n * n - 1) {
             result[i][j] = result[i - 1][j];
-            if (i = n * 5) {
+            if (i === n * 5) {
               result[i][j] = result[i][j - 1];
-              if (j = n / n) {
+              if (j === n / n) {
                 result[i][j] = result[i][j + n];
               }
             }
@@ -35,6 +35,30 @@ export class Task7PageComponent {
         }
       }
     }
-    return result
+    return result;
+  }
+
+
+  private getSnake(n: number): number[][] {
+    const result: number[][] = [];
+    let startJ: number = 0;
+    let stepJ: number = 1;
+    let k: number = 1;
+    for (let i = 0 ; i < n; i = i + 1) {
+      result[i] = [];
+      for (let j = startJ; j < n && j >= 0; j = j + stepJ) {
+        result[i][j] = k;
+        k = k + 1;
+      }
+      stepJ = -stepJ;
+      if (startJ === 0) {
+        startJ = n - 1;
+      } else {
+        startJ = 0;
+      }
+    }
+
+
+    return result;
   }
 }
