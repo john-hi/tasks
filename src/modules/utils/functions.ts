bcd12009804b7f232a60a07fda50a98ea16ce4c3
enigma812 <antiseptic812@gmail.com>
2022-06-20 16:59:54 +0300

    feat(theory): high ordered function

---
+++ src/modules/utils/functions.ts
@@ -0,0 +1,71 @@
+function srq(x: number): number {
+    return x * x;
+    // return x**2;
+    // return Math.pow(x, 2);
+}
+
+// const arrowSqr: (x: number) => number = (x: number) => {
+const arrowSqr = (x: number) => {
+    return x * x;
+}
+
+function map(array: number[], mapFn: (value: number) => number) {
+    // const result: number[] = [];
+    // for(let i = 0; i < array.length; i++) {
+    //     result.push(mapFn(array[i]));
+    // }
+    // return result;
+    return array.map(mapFn);
+}
+
+
+function add(term: number): (value: number) => number {
+    const result: (value: number) => number = (value) => value + term;
+    return result;
+    return (value) => value + term;
+}
+
+function testAdd(): void {
+    const threePlusTwo = add(2)(3);
+}
+
+
+function arraySum(arr: number[]): number {
+    let sum: number = 0;
+    // const sumFn = (value: number) => {
+    //     sum = sum + value
+    // };
+    // arr.forEach(sumFn);
+    arr.forEach((value) => {
+        sum = sum + value;
+    });
+    return sum;
+}
+
+function getOdds(array: number[]): number[] {
+    return array.filter((value) => {
+        // if(value % 2 === 0) {
+        //     return false;
+        // } else {
+        //     return true;
+        // }
+        return value % 2 === 0;
+    });
+    return array.filter((value) => value % 2 === 1);
+}
+
+
+function getDoubles(array: number[]): number[] {
+    return array.map((value: number) => value * 2);
+}
+
+
+function contains(array: number[], search: number): boolean {
+    const found = array.find((value: number) => value === search);
+    return found !== undefined;
+
+    return array.includes(search);
+}
+
+
+
