c8c0582cd37cc73a573a7e9d101057d1e2c00c09
enigma812 <antiseptic812@gmail.com>
2022-06-21 16:29:39 +0300

    feat(theory): generics

---
+++ src/modules/utils/types-theory.ts
@@ -0,0 +1,61 @@
+// примитивы
+
+let b: boolean; // false или true
+let n: number; // число целове или дробное
+let s: string; // строка
+let a: any;  // любой тип
+
+let array: number[]; // числовой массив []
+let obj: { id: number, name: string }; // объекты со свойствами и методами
+
+let fn: (x: number) => number; // функция
+
+
+
+//создание своих типов Union types
+
+type StringOrNumber = string | number; // перечисление типов (или то или другое)
+let sn: StringOrNumber;
+type State = 'defined' | 'undefined';
+type Order = 42 | 58 | 37 | 25;
+let o: Order = 37;
+
+type Id = { id: number };
+type Name = { name: string };
+type IdName = Id & Name;           // объединение типов (и то и другое)
+let objIdName: IdName;
+objIdName = { id: 42, name: 'John' };
+
+
+
+//Литеральные типы   не общие примитивы (string, number, boolean), а конкретные значения
+
+type Success = 'success';
+type Failed = 'failed';
+type Status = Success | Failed;
+let st: Status = 'success'
+
+
+// Обобщенные типы (генерик)
+
+type Arr<T> = T[];
+let numbers: Arr<number>; // numbers: number[]
+let strings: Arr<string>; // strings: string[]
+
+//генерик объектов
+
+type GenIdName<TId, TName> = { id: TId, name: TName };
+let genIdName: GenIdName<number, string> = { id: 42, name: 'John' };
+
+let genIdName2: GenIdName<string, 'John' | 'Sarah'> = { id: 'dsfsd', name: 'John' };
+
+// генерик функций
+
+type SortFn<T> = (array: T[]) => T[];
+
+const sortNumbers: SortFn<number> = (arr) => arr.sort();
+const sortStrings: SortFn<string> = (arr) => arr.sort();
+
+
+
+
